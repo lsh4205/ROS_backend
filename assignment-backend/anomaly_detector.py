d7ab09606b76a920655c72f4af428ecb6229f636
lsh4205 <63761734+lsh4205@users.noreply.github.com>
2023-05-10 07:52:31 -0400

    Update anomaly_detector.py
    Created AnomalyDetector class

--- assignment-backend/anomaly_detector.py
+++ assignment-backend/anomaly_detector.py
@@ -7,3 +7,17 @@ ANOMALY_VELOCITY_THRESHOLD = 4.5
 DELTA_T = 2.0
 SITE_NAME = "BMW"
 BAG_FILE = "data.bag"
+
+class AnomalyDetector:
+    def __init__(self):
+        self.anomalies =[]
+        self.point_clouds = []
+
+    def process_rosbag_file(self):
+        bag = rosbag.Bag(BAG_FILE, "r")
+        te = True
+        for topic, msg, t in bag.read_messages():
+            if topic == "/velocity":
+                self.velocity_callback(msg, t.to_sec())
+            elif topic == "/lidar_0000/os_cloud_node/points":
+                self.point_clouds.append((t.to_sec(), msg))
